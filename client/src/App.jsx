4b7e05911622af64046704a38018221300753946
Whitney-Rene <whtnchambers76@gmail.com>
2023-09-06 14:22:38 -0400

    creates txt files to save practice code and deletes that code from quest comp and app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,33 +3,11 @@ import './App.css'
 import Question from './components/question';
 
 function App() {
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
-  const [test, setTest] = useState('');
+
   const [question, setQuestion] = useState('');
 
-    const callForName = async () => {
-      const response = await fetch ('/myAPI/myName');
-      const data = await response.json();
-      console.log(data);
-      setName(data.name);
-    }
-     const callForMessage = async () => {
-      const response = await fetch ('myAPI/');
-      const data = await response.json();
-      console.log(data);
-      setMessage(data.message);
-     }
-
-     const callForQuiz = async () => {
-      const response = await fetch ('http://localhost:1234/test');
-      const data = await response.json();
-      console.log(data);
-      setTest(data.test);
-     }
-
      const callForQuizAPI = async () => {
-      const response = await fetch ('http://localhost:1234/quizApi');
+      const response = await fetch ('/myAPI/quizApi');
       const data = await response.json();
       console.log(data);
       setQuestion(data.results[0].question);
@@ -38,18 +16,12 @@ function App() {
     //to see name as soon as page renders, you need a hook, useEffect
     //function that takes an anonymous function=first param=what do I want to happen when page renders
     useEffect(() => {
-      callForName();
-      callForMessage();
-      callForQuiz();
       callForQuizAPI();
     }, []);
 
   return (
     <>
     <div>
-      <h1>Hi {name}</h1>
-      {message}
-      {test}
       <Question question={question}/>
     </div>
     </>
